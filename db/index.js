bfb8f7186ee6c229c1e6ffd8bbda7bebfd2e46d4
D Kornbluth <dwight.kornbluth@gmail.com>
2021-10-28 06:37:39 -0400

    added all functions in index.js regarding user option/choice

--- db/index.js
+++ db/index.js
@@ -6,7 +6,44 @@ class orgDB {
         this.connection = connection;
     }
 
-  // Find all employees, join with roles and departments to display their roles, salaries, departments, and managers
+    // Find all departments
+    getDepartments() {
+        return this.connection.promise().query(
+        "SELECT department.id, department.name FROM department;"
+        );
+    }
+
+    // Create a new department
+    createDepartment(department) {
+        return this.connection.promise().query("INSERT INTO department SET ?", department);
+    }
+
+    // Remove a department
+    removeDepartment(departmentId) {
+        return this.connection.promise().query(
+        "DELETE FROM department WHERE id = ?",
+        departmentId
+        );
+    }  
+
+    // Find all roles, join with departments to display the department name
+    getRoles() {
+        return this.connection.promise().query(
+        "SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department on role.department_id = department.id;"
+        );
+    }
+
+    // Create a new role
+    createRole(role) {
+        return this.connection.promise().query("INSERT INTO role SET ?", role);
+    }
+
+    // Remove a role from the db
+    removeRole(roleId) {
+        return this.connection.promise().query("DELETE FROM role WHERE id = ?", roleId);
+    }
+
+    // Find all employees, join with roles and departments to display their roles, salaries, departments, and managers
     getEmployees() {
         return this.connection.promise().query(
           "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
@@ -24,23 +61,6 @@ class orgDB {
         "DELETE FROM employee WHERE id = ?",
         employeeId
         );
-    }
-
-    // Find all roles, join with departments to display the department name
-    getRoles() {
-        return this.connection.promise().query(
-        "SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department on role.department_id = department.id;"
-        );
-    }
-
-    // Create a new role
-    createRole(role) {
-        return this.connection.promise().query("INSERT INTO role SET ?", role);
-    }
-
-    // Remove a role from the db
-    removeRole(roleId) {
-        return this.connection.promise().query("DELETE FROM role WHERE id = ?", roleId);
     }
 
     // Find all employees except the given employee id
@@ -67,25 +87,6 @@ class orgDB {
         );
     }
 
-    // Find all departments
-    getDepartments() {
-        return this.connection.promise().query(
-        "SELECT department.id, department.name FROM department;"
-        );
-    }
-
-    // Create a new department
-    createDepartment(department) {
-        return this.connection.promise().query("INSERT INTO department SET ?", department);
-    }
-
-    // Remove a department
-    removeDepartment(departmentId) {
-        return this.connection.promise().query(
-        "DELETE FROM department WHERE id = ?",
-        departmentId
-        );
-    }
 }
 
 module.exports = new orgDB(connection);
